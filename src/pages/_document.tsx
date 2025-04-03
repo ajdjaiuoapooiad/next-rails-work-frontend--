5a043327e3830cd06c6c30715230d06b5f31531b
yukito <iyggf66974@yahoo.ne.jp>
2025-04-03 19:31:09 +0900

    deploy

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="ja">
       <Head>
         <link rel="icon" href="/images/logo2.svg" />
         <title>インターンシップマッチングアプリ</title> {/* 共通のタイトルを設定 */}
